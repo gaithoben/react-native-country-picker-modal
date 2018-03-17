8d16ba31aab1310f2841730242a7f30f6c3b797d
Bernard Gaitho <gaithoben@gmail.com>
2018-03-17 10:34:36 +0300

    Adding Animated Modal

--- src/CountryPicker.js
+++ src/CountryPicker.js
@@ -4,13 +4,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import SafeAreaView from 'react-native-safe-area-view';
+import Modal from 'react-native-modal';
 
 import {
   StyleSheet,
   View,
   Image,
   TouchableOpacity,
-  Modal,
   Text,
   TextInput,
   ListView,
